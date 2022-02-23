da35eeb8156e9ae26163643ff9e1cf3a7e742894
irinapichalka <irinarescan@gmail.com>
2022-02-23 15:35:15 +0200

    export

--- DOM/homework1/index.js
+++ DOM/homework1/index.js
@@ -1,9 +1,9 @@
-function getTitleElement() {
+export function getTitleElement() {
   const titleElem = document.querySelector(".title");
   console.dir(titleElem);
   return titleElem;
 }
-function getInputElement() {
+export function getInputElement() {
   const inputElem = document.querySelector('input[type="text"]');
   console.dir(inputElem);
   return inputElem;
