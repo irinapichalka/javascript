2a906f38127bd5d2cdd40adba90a0ba58c942728
irinapichalka <irinarescan@gmail.com>
2022-02-23 15:54:48 +0200

    homework3 js changed

--- DOM/homework3/index.js
+++ DOM/homework3/index.js
@@ -6,5 +6,15 @@ export function getDescription() {
   const inputElem = document.querySelector(".about");
   return inputElem.innerText;
 }
+export function getPlans() {
+  const inputElem = document.querySelector(".plans");
+  return inputElem.innerText;
+}
+export function getGoal() {
+  const inputElem = document.querySelector(".goal");
+  return inputElem.outerText;
+}
 getTitle();
 getDescription();
+getPlans();
+getGoal();
