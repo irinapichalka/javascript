64ba988a8f05d22c6205a9db6d9e5e4f80b9d7e8
irinapichalka <irinarescan@gmail.com>
2022-10-21 14:28:23 +0300

    http

---
+++ HTTPS/index.js
@@ -0,0 +1,31 @@
+const userAvatarElem = document.querySelector(".user__avatar");
+const userNameElem = document.querySelector(".user__name");
+const userLocationElem = document.querySelector(".user__location");
+
+const defaultAvatar = "google.jpg";
+userAvatarElem.src = defaultAvatar;
+
+const fetchUserData = (userName) => {
+  return fetch(`https://api.github.com/users/${userName}`).then((response) =>
+    response.json()
+  );
+};
+
+const renderUserData = (userData) => {
+  const { avatar_url, name, location } = userData;
+  console.log(userData);
+  userAvatarElem.src = avatar_url;
+  userNameElem.textContent = name;
+  userLocationElem.textContent = location ? `from ${location}` : ``;
+};
+
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
+
+const onSearchUser = () => {
+  const userName = userNameInputElem.value;
+  fetchUserData(userName);
+  console.log(userName);
+};
+
+showUserBtnElem.addEventListener("click", onSearchUser);
