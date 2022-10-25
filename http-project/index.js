b9657e340f6ba7c972ab189270216e2b99d2de73
irinapichalka <irinarescan@gmail.com>
2022-10-25 14:25:16 +0300

    http

--- http-project/index.js
+++ http-project/index.js
@@ -1,10 +1,11 @@
 const baseUrl = "https://635674979243cf412f8501b0.mockapi.io/api/v1/user";
 
 const formElem = document.querySelector(".login-form");
-
 const submitBtnElem = document.querySelector(".submit-button");
 
-submitBtnElem.removeAttribute("disabled");
+if (formElem.reportValidity()) {
+  submitBtnElem.removeAttribute("disabled"); //?????
+}
 
 function createUser(userData) {
   return fetch(baseUrl, {
@@ -17,13 +18,7 @@ function createUser(userData) {
 }
 function onSubmitData() {
   const formData = Object.fromEntries(new FormData(formElem));
-  console.log(formData);
-  createUser(formData).then((data) => console.log(data));
-}
-function getUser() {
-  return fetch(baseUrl)
-    .then((data) => data.json())
-    .then((data) => console.log(data));
+  createUser(formData).then((userData) => console.log(userData));
 }
 
-submitBtnElem.addEventListener("click", onSubmitData);
+submitBtnElem.addEventListener("submit", onSubmitData);
